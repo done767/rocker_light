1906dc3ff50e4ca24050b1efe0e9830b298c8b76
hm <hm@hms-MacBook-Pro.local>
2020-04-28 17:04:19 +0900

    NaN

---
+++ library/install.sh
@@ -0,0 +1,24 @@
+sudo apt-get update -y
+sudo apt-get install libssh2-1-dev -y # git2r
+sudo apt-get install zlib1g-dev    -y # git2r
+sudo apt-get install libmpfr-dev -y # Bessel
+sudo apt-get install libgmp3-dev -y # Bessel
+sudo apt-get install libxt-dev -y # Cairo
+sudo apt-get install libcairo2-dev -y  # Cairo
+sudo apt install libgsl-dev -y  # MBESS
+sudo apt-get install jags -y  # R2jags
+sudo apt-get install libmysql++-dev -y  # RMySQL
+sudo apt-get install libpq-dev -y  # RPostgreSQL
+sudo apt-get install libopenmpi-dev -y #Rmpi
+sudo apt-get install tk-dev -y  # ade4TkGUI
+sudo apt-get install libxml2-dev -y  # xml2
+sudo apt-get install libjpeg-dev -y  # imager
+sudo apt-get install libtiff5-dev -y  # imager
+sudo apt-get install libudunits2-dev -y  # units
+sudo apt-get install libgdal-dev -y  # sf
+sudo apt-get install r-cran-rjava -y  # scagnostics
+sudo apt-get install openjdk-8-jdk -y  # rJava
+sudo R CMD javareconf -y  # rJava
+sudo apt-get install libx11-dev mesa-common-dev libglu1-mesa-dev -y  # rgl
+sudo apt-get install -y libmagick++-dev # magick
+sudo apt-get install libopenbabel-dev -y # ChemmineOB
--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ docker rm   rocker
 open http://localhost:8787
 docker run -d --restart always -v $(pwd)/share:/home/rstudio/kitematic --name rocker -p 8787:8787 -e DISABLE_AUTH=true rocker/rstudio
 
-docker cp library/install.R rocker:/home/
-docker exec -it rocker Rscript /home/install.R
-
 docker cp library/install.sh rocker:/home/
 docker exec -it rocker chmod 777 /home/install.sh
 docker exec -it rocker /bin/bash /home/install.sh
+
+docker cp library/install.R rocker:/home/
+docker exec -it rocker Rscript /home/install.R
